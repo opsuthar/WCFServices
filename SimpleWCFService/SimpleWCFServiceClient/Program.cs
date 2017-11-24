7bc1f0da5ce0d0c505011c974696c64625cd39e3
opsuthar <opsuthar121@gmail.com>
2017-11-24 06:12:20 +0530

    Client reading student name successful

--- SimpleWCFService/SimpleWCFServiceClient/Program.cs
+++ SimpleWCFService/SimpleWCFServiceClient/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SimpleWCFServiceClient.SimpleWCFServiceReference;
+
 namespace SimpleWCFServiceClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            StudentServiceClient client = new StudentServiceClient();
+
+            var name = client.GetName();
+
+            Console.WriteLine("Name of the student is " + name);
         }
     }
 }
